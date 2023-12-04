5828f88343eeeed7d5e0213665d3e07661cc76b9
outerLeitmotiv <olivier.mouttet@gmail.com>
2023-12-04 15:45:30 +0100

    adding signin and signup TODO: use loadScene

--- app/src/main/java/ch/hearc/ig/citymobis/MainActivity.kt
+++ app/src/main/java/ch/hearc/ig/citymobis/MainActivity.kt
@@ -2,14 +2,23 @@ package ch.hearc.ig.citymobis
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.storage.*
 import io.github.sceneview.SceneView
 import io.github.sceneview.loaders.ModelLoader
@@ -21,10 +30,97 @@ import kotlinx.coroutines.withContext
 import java.nio.ByteBuffer
 
 class MainActivity : ComponentActivity() {
+    private lateinit var sceneView: SceneView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setContent {
+            AppContent()
+        }
+    }
+
+    @Composable
+    fun AppContent() {
+        val isAuthenticated = remember { mutableStateOf(FirebaseAuth.getInstance().currentUser != null) }
+
+        if (isAuthenticated.value) {
+            MainContent()
+        } else {
+            AuthenticationUI { email, password ->
+                signInOrSignUp(email, password, isAuthenticated)
+            }
+        }
+    }
+
+    @Composable
+    fun MainContent() {
+        val context = LocalContext.current
+        sceneView = SceneView(context)
+        val storage = FirebaseStorage.getInstance()
+        val modelRef = storage.reference.child("futuristic_building.glb")
+
+        Box(modifier = Modifier.fillMaxSize()) {
+            SceneViewContainer(modelRef)
+        }
+    }
+
+    @Composable
+    fun AuthenticationUI(onAuthentication: (String, String) -> Unit) {
+        var email by remember { mutableStateOf("") }
+        var password by remember { mutableStateOf("") }
+        val context = LocalContext.current
+
+        Column(modifier = Modifier.padding(16.dp)) {
+            TextField(
+                value = email,
+                onValueChange = { email = it },
+                modifier = Modifier.padding(bottom = 8.dp),
+                label = { Text("Email") }
+            )
+            TextField(
+                value = password,
+                onValueChange = { password = it },
+                modifier = Modifier.padding(bottom = 16.dp),
+                label = { Text("Password") },
+                visualTransformation = PasswordVisualTransformation()
+            )
+            Button(
+                onClick = {
+                    if (email.isNotBlank() && password.isNotBlank()) {
+                        onAuthentication(email, password)
+                    } else {
+                        Toast.makeText(context, "Please enter both email and password", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            ) {
+                Text("Sign In/Sign Up")
+            }
+        }
+    }
+
+
+    private fun signInOrSignUp(email: String, password: String, isAuthenticated: MutableState<Boolean>) {
+        val auth = FirebaseAuth.getInstance()
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                isAuthenticated.value = true
+            } else {
+                // If sign in fails, try sign up
+                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { signUpTask ->
+                    if (signUpTask.isSuccessful) {
+                        isAuthenticated.value = true
+                    } else {
+                        Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun loadScene() {
+        val context = this
+        sceneView = SceneView(context)
         val storage = FirebaseStorage.getInstance()
         val modelRef = storage.reference.child("futuristic_building.glb")
 
