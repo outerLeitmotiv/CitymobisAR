ba8efb02ffc17490a7c08c3612b9f890e12a3e58
outerLeitmotiv <olivier.mouttet@gmail.com>
2023-12-04 13:38:24 +0100

    reworked app to work with buffer, black screen

--- app/src/main/java/ch/hearc/ig/citymobis/MainActivity.kt
+++ app/src/main/java/ch/hearc/ig/citymobis/MainActivity.kt
@@ -1,120 +1,73 @@
 package ch.hearc.ig.citymobis
 
 import android.os.Bundle
-import android.os.Environment
 import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLifecycleOwner
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.storage.FirebaseStorage
-import io.github.sceneview.Scene
+import androidx.compose.ui.viewinterop.AndroidView
+import com.google.firebase.storage.*
+import io.github.sceneview.SceneView
 import io.github.sceneview.loaders.ModelLoader
 import io.github.sceneview.node.ModelNode
-import io.github.sceneview.node.Node
-import io.github.sceneview.rememberEngine
-import io.github.sceneview.rememberModelLoader
-import io.github.sceneview.rememberNodes
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
-import java.io.File
-
-
+import java.nio.ByteBuffer
 
 class MainActivity : ComponentActivity() {
 
-    lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        auth = FirebaseAuth.getInstance()
-        signInStaticUser()
+        // Initialize Firebase
+        val storage = FirebaseStorage.getInstance()
+        val modelRef = storage.reference.child("ImageToStl.com_eglise_rouge3.glb")
 
         setContent {
-            val modelLoader = rememberModelLoader(rememberEngine())
-            val nodes = rememberNodes()
-            val localFilePath = "${filesDir}/eglise_rouge3.glb"
-
-            ModelScreen()
-            LaunchedEffect(true) {
-                withContext(Dispatchers.IO) {
-                    downloadFile()
-                    loadModel(nodes, modelLoader, localFilePath)
-                }
+            Box(modifier = Modifier.fillMaxSize()) {
+                SceneViewContainer(modelRef)
             }
         }
     }
+
     @Composable
-    fun ModelScreen() {
-        val nodes = rememberNodes()
+    fun SceneViewContainer(modelRef: StorageReference) {
+        val context = LocalContext.current
+        val sceneView = remember { SceneView(context) }
+        val engine = sceneView.engine
+        val modelLoader = ModelLoader(engine, context)
 
-        Box(modifier = Modifier.fillMaxSize()) {
-            Scene(
-                activity = LocalContext.current as? ComponentActivity,
-                lifecycle = LocalLifecycleOwner.current.lifecycle,
-                childNodes = nodes,
-                engine = rememberEngine(),
-            )
-        }
-    }
-
-    private fun loadModel(nodes: SnapshotStateList<Node>, modelLoader: ModelLoader, modelPath: String) {
-        val modelNode = ModelNode(
-            modelInstance = modelLoader.createModelInstance(modelPath),
-            autoAnimate = true
-        )
-        nodes.add(modelNode)
-    }
-
-    private fun downloadFile() {
-        val storage = FirebaseStorage.getInstance()
-        val storageRef = storage.getReferenceFromUrl("gs://citymobis-89b20.appspot.com")
-        val islandRef = storageRef.child("ImageToStl.com_eglise_rouge3.glb")
-
-        val rootPath = File(Environment.getExternalStorageDirectory(), "file_name")
-        if (!rootPath.exists()) {
-            rootPath.mkdirs()
-        }
-
-        val localFile = File(rootPath, "eglise_rouge3.glb")
-
-        islandRef.getFile(localFile)
-            .addOnSuccessListener { taskSnapshot ->
-                Log.e("firebase", ";local temp file created: ${localFile.absolutePath}")
-                // updateDb(timestamp, localFile.toString(), position)
-            }
-            .addOnFailureListener { exception ->
-                Log.e("firebase", ";local temp file not created: ${exception.message}")
-            }
-    }
-
-    private fun signInStaticUser() {
-        val email = "user@name.com"
-        val password = "password"
-
-        auth.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success
-                    Log.d("FirebaseAuth", "signInWithEmail:success")
-                    val user = auth.currentUser
-                    // Proceed with your app logic here...
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w("FirebaseAuth", "signInWithEmail:failure", task.exception)
-                    Toast.makeText(baseContext, "Authentication failed.",
-                        Toast.LENGTH_SHORT).show()
+        LaunchedEffect(modelRef) {
+            try {
+                val buffer = downloadModel(modelRef)
+                buffer?.let {
+                    val modelNode = ModelNode(modelInstance = modelLoader.createModelInstance(it))
+                    sceneView.addChildNode(modelNode)
+                    // Optionally, force an update or redraw of the SceneView here
                 }
+            } catch (e: Exception) {
+                Log.e("MainActivity", "Error in model loading: ${e.message}")
             }
+        }
+
+        AndroidView({ sceneView })
     }
 
+
+    private suspend fun downloadModel(modelRef: StorageReference): ByteBuffer? {
+        return try {
+            withContext(Dispatchers.IO) {
+                val bytes = modelRef.getBytes(Long.MAX_VALUE).await()
+                ByteBuffer.wrap(bytes)
+            }
+        } catch (e: Exception) {
+            Log.e("MainActivity", "Error downloading model: ${e.message}")
+            null
+        }
+    }
 }
